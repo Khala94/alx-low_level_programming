a6b1638f3eb39a1bf4ce9b8b68f69515a2de146d
Ofentse <surprise.ofentse250@gmail.com>
2023-02-16 17:33:54 -0800

    Task 4: puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#import  <stdio.h>
-main(void)
+#include <stdio.h>
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n")
-	return(0)
+	puts("Programming is like building a multilingual puzzle/n");
+	return(0);
 }
