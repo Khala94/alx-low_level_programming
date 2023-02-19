765dab2c671631892c5e5f2b5d1fcba3e52c6f57
Ofentse <surprise.ofentse250@gmail.com>
2023-02-19 00:23:42 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
-* Main - "Print sizes of data"
-* Description: "Program should print sizes for char, int  long int"
-* Return: 0
-*/ 
+* main - Program prints different data sizes
+* description: "Print sizes for chqr, int, long int, long long int"
+* reutn: 0
+*/
 int main(void)
 {
-        printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int);
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int);
+	printf("Size of a float: %ld byte(s)\n", sizeof(float);
+
 	return (0);
 }
