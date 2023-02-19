f89610d9cf02a0e08566f89da6a9287ef84922af
Ofentse <surprise.ofentse250@gmail.com>
2023-02-18 23:11:54 -0800

    Task 5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+* Function main should print provided values
+* Print values if program returns 0
+*
+*/
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
