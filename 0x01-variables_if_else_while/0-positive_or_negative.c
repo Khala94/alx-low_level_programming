14f45c5fd4402d79d495916bf9d46538004a6f03
Ofentse <surprise.ofentse250@gmail.com>
2023-02-20 19:04:09 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
 * Main - Start of the program
@@ -25,5 +26,5 @@ int main(void)
 	{
 		printf("%d is zero", n);
 	}
-	reutn (0);
+	return (0);
 }
