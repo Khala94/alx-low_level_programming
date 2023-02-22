3c6d498ba30996b52247968011d89256193bbc8c
Ofentse <surprise.ofentse250@gmail.com>
2023-02-21 18:21:08 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* All headers goes here*/
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
