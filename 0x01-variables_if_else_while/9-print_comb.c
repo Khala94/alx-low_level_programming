27e63f989715dd99bc4551f5ae2f3e8851d68109
Ofentse <surprise.ofentse250@gmail.com>
2023-02-21 18:50:35 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* All my headers goes here */
+
+/** 
+* main - Entry Point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+
+		if (x != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		putchar(100);
+		return (0);
+	}
+}
