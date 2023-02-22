903692271f33c09f6b9e611a91a23c8a149a36d0
Ofentse <surprise.ofentse250@gmail.com>
2023-02-21 18:30:52 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* all headers goes here */
+
+/**
+* main - Entry point
+* Return: Alwqys 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar(10)
+	return (0);
+}
