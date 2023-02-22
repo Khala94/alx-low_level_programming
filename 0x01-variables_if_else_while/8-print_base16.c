174561407d7cd6ea3d88c30d3832fcb1f312cdb8
Ofentse <surprise.ofentse250@gmail.com>
2023-02-21 18:43:10 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* All mybheaders goes herr */
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'g'; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
