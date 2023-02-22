5b9d1ead0bc5538e1c2ade2f5f24b8e0d652bbee
Ofentse <surprise.ofentse250@gmail.com>
2023-02-21 17:32:32 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* All headers goes here */
+
+/**
+* Main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+	for (x = 'a'; x<= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
