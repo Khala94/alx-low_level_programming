73301bf10db0c81be27e7da70a0dd4bab6ec52c4
Ofentse <surprise.ofentse250@gmail.com>
2023-02-23 08:52:24 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,14 +6,18 @@
 * Return: Always 0
 */
 
-int main(void){
+int main(void)
+{
 
-	int _isupper(int c){
+	int _isupper(int c)
+	{
 
-		if (c >= 'A' && c <= 'Z'){
+		if (c >= 'A' && c <= 'Z')
+		{
 			return (1);
 		}
-		else {
+		else
+		{
 			return (0);
 		}
 
