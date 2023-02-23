647df1026587266abc7bebcd6f1679c070769964
Ofentse <surprise.ofentse250@gmail.com>
2023-02-23 09:33:26 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* all headers goes here */
+
+/**
+* mul: will multiply two digits
+* @c: provided value
+* Return: Always 0
+*/
+
+int mul(int a, int b)
+{
+	int answer;
+
+	answer = (a * b);
+	return (c);
+}
