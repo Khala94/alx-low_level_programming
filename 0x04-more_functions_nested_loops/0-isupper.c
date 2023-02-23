d22bfb3d48a83d397a533df29dcaac6841ae2264
Ofentse <surprise.ofentse250@gmail.com>
2023-02-23 08:24:31 -0800

    Task 0 uppercase checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/* All my headers goes here*/
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void){
+
+	int _isupper(int c){
+
+		if (c >= 'A' && c <= 'Z'){
+			return (1);
+		}
+		else {
+			return (0);
+		}
+
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Read me file for more functiins nested loops task 4 under low level programming
