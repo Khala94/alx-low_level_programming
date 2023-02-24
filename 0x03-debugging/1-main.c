2dffd4912a28be2a269922e28d77f47cd94ee119
Ofentse <surprise.ofentse250@gmail.com>
2023-02-23 17:31:38 -0800

    Task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - infinite loop
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	prinf("Infinite loop incoming:\n");
+
+	i = 0;
+
+	printf("Iinfinite loop avoided! \\o/\n");
+
+	return (0);
+}
