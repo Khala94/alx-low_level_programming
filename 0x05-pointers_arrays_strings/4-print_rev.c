7aa29fbd9e608a382901e0d0c0d59fd8d17ecb79
Ofentse <surprise.ofentse250@gmail.com>
2023-02-28 15:47:10 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#includw "main.h"
+/* All my headers goes gere */
+
+/**
+* print_rev - Print string in reverse
+* @s: string to be reversed
+* Return: Reversed string
+*/
+
+void print_rev(char *s)
+{
+	int x, i;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+		for (i = x - 1; i >= 0; i--)
+		{
+			_putchar([i]);
+			_putchar(10);
+		}
+	}
+}
