4fbf2cce4093c39b68843103660050ac0753e94c
Ofentse <surprise.ofentse250@gmail.com>
2023-02-28 16:10:33 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* All my headers goes here */
+
+/**
+* rev_string - Entry Point
+* Description: Program should print variable in reverse
+* @s: variable to be printed
+* return: reversed variable
+*/
+
+void rev_string(char *s)
+{
+	int a, b;
+
+	a = 0;
+
+	b = 0;
+
+	while (s[b++])
+	{
+		a++;
+
+		for (b	= b - 1; b >= b / 2; b--)
+		{
+			char x = s[b];
+
+			s[b] = s[a - b - 1];
+			s[a - b - 1] = x;
+		}
+	}
+}
