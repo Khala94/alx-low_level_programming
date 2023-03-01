139fbf8124081d80f3f84d16eccceac938e26394
Ofentse <surprise.ofentse250@gmail.com>
2023-02-28 16:40:38 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/* All my headers goes here */
+
+/**
+* print_array - Entry point
+* Description: Print numbers in a array
+* @n: The array to print
+* Return: The array
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (a[i] != -1024)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+			i++;
+		}
+	}
+	printf("\n");
+}
