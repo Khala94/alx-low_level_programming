8086667e5db1203ff6b1d05b8e714df694012c5e
Ofentse <surprise.ofentse250@gmail.com>
2023-03-02 18:05:45 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	init l = 0;
+	int l = 0;
 
 	while (src[i++])
 		l++;
 	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[j];
+		dest[i] = src[l];
 	for (i = l; i < n; i++)
 		dest[i] != '\0';
 	return (dest);
