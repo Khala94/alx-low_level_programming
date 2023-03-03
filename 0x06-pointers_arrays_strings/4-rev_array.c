a84b805b7781bf527cf41dd11b8f03cb6ab605d4
Ofentse <surprise.ofentse250@gmail.com>
2023-03-02 18:24:31 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int l;
+	int c;
 
-	for (i = n - 1, i >= n / 2; i++)
+	for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		l = a[n - 1];
-		a[n - 1 - i] = a[i];
-		a[i] = l;
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
 	}
 }
