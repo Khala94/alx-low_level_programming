ae6a3ac1117d6b732c6fc14643c197fdf358f349
Ofentse <surprise.ofentse250@gmail.com>
2023-03-02 18:26:59 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,10 @@
 * Return: values
 */
 
-int _strcmp(char *s11, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int result;
 
-	while (s1[i] == s2[i])
-	{
-		if (s1[i] == '\0' || s2[i] == '\0')
-		{
-			break;
-			i++;
-		}
-	}
-
-	if (s1[i] < s2[i])
-	{
-		return (-15);
-	}
-	else if (s1[i] > s2[i])
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	result = strcmp(s1, s2);
+	return (result);
 }
