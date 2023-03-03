f1d4f95728a5c148338519ce090827772f706e64
Ofentse <surprise.ofentse250@gmail.com>
2023-03-02 18:54:39 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /* All my headers goes here */
 
 /**
@@ -11,14 +12,6 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	int l = 0;
-
-	while (src[i++])
-		l++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[l];
-	for (i = l; i < n; i++)
-		dest[i] != '\0';
+	strncpy(dest, src, n);
 	return (dest);
 }
