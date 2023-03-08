bf4bfee5d200619d5b4620ed5f8889e185cf5568
Ofentse <surprise.ofentse250@gmail.com>
2023-03-07 17:18:09 -0800

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* All my headers goes here */
+
+/**
+* _strpbrk - Entry point
+* @s: input
+* accept: input
+* Return:Always 0
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return ('\0');
+}
