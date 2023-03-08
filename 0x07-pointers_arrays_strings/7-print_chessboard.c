315bc9f93c20779869b92dd73c2b0c89c60e3f5b
Ofentse <surprise.ofentse250@gmail.com>
2023-03-07 17:56:46 -0800

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, x = 0;
+	int i, j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (j = 0; j < 8; j++)
 		{
-			x = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
 		}
-		_putchar(a[i / 8][i - x]);
+		_putchar('\n');
 	}
 }
