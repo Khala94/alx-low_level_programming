97fe734d80e2f0a2a71e4a692c1bb014e7401a85
Ofentse <surprise.ofentse250@gmail.com>
2023-03-09 16:38:55 -0800

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* All my headers goes here */
+
+/**
+* find_sqrt - Finds the natral square on inputed number
+* @num: Finding square of this variable
+* @root: The root
+* Return: 1 if the number has natural squares and 0 if it doesnt
+*/
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+	{
+		return (root);
+	}
+	else if (root == num / 2)
+	{
+		return (-1);
+	}
+	return (find_sqrt(num, root + 1));
+}
