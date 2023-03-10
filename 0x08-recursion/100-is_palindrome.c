dd5b711404f2387b3d6c22f49c555c3f701938cd
Ofentse <surprise.ofentse250@gmail.com>
2023-03-09 17:10:10 -0800

    Hot fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,6 +8,10 @@
  * return: The length of the string
  */
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int  index);
+int is_palindrome(char *s);
+
 int find_strlen(char *s)
 {
 	int len = 0;
