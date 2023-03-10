97a65843b549ba70e7b07482f64b25581610deac
Ofentse <surprise.ofentse250@gmail.com>
2023-03-09 17:02:17 -0800

    Task 7

---
+++ 0x08-recursion/100-is-palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/* All my headers goes here */
+
+/**
+ * find_strlen - Entry point
+ * description: Find length of a string variable
+ * @s: String to be measured
+ * return: The length of the string
+ */
+
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
+
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+	{
+		return (1);
+	}
+	else if (s[index] == s[len - index - 1])
+	{
+		return (check_palindromes(s, len, index + 1));
+	}
+	return (0);
+}
+
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+	{
+		return (1);
+	}
+	return (check_palindrome(s, len, index));
+}
