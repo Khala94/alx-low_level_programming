d6af172b24c6965f8bdcf49aaaed8bde2382ed5e
Ofentse <surprise.ofentse250@gmail.com>
2023-03-22 02:06:56 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* All my headers goes here */
+
+/**
+* main - entry point
+* description: Print the name of the file
+* return: Always 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
