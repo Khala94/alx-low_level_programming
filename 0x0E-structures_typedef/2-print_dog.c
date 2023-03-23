739befa948e8dfa8e3d12b7985791b93b0bcc84e
Ofentse <surprise.ofentse250@gmail.com>
2023-03-22 17:09:16 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,21 +13,12 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-		{
 			printf("Name: %s\n", d->name);
-		}
 		else
-		{
 			printf("Name: (nil)\n");
-		}
-
 		if (d->owner != NULL)
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 		else
-		{
-			printf("Owner: (nil)\n");
-		}
+			printf("Owner: (nil)\n");		
 	}
 }
