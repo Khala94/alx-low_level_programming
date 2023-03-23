10aa07137117a7352ab726519799721c4de5332d
Ofentse <surprise.ofentse250@gmail.com>
2023-03-22 17:05:39 -0700

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dog.h"
+/* All my headers goes here */
+
+/**
+* print_dog - prints a struct dog
+* @d: pointer to structure
+* return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
